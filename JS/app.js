a80e139cf94689627555abf7261b78a3c806e8c9
Hernan Marmarides <hernan.marmarides@gmail.com>
2022-06-29 18:57:05 -0300

    avance

--- JS/app.js
+++ JS/app.js
@@ -6,6 +6,7 @@ const contadorPedido = document.querySelector ('#contador-pedido')
 const precioTotal = document.querySelector ('#precio-total')
 const contenedorClass = document.querySelector ('#contenedor-menu')
 
+
 // CREACION DE PRODUCTOS EN JS
 
 menuPizza.forEach((producto) => {
@@ -22,65 +23,87 @@ menuPizza.forEach((producto) => {
   contenedorClass.appendChild(div)
 })
 
-
-contenedorClass.className= ('galery')
-
-
-
-
 // SELECCION DEL PRODUCTO
 const agregarPedido = (id) => {
   let item = menuPizza.find ((producto) => producto.id === id)
 
+  // localStorage.setItem ('pedido', JSON.stringify(pedido))
+  
   pedido.push(item)
-  console.log(pedido)
 
   renderPedido()
   renderCantidad()
- 
+  renderTotal()
+}
+
+//RENDERIZADO DE LA CANTIDAD SELECCIONADA EN CARRITO
+
+const renderCantidad = () => {
+  contadorPedido.innerText = pedido.length
+  
+}
+
+const renderTotal = () => {
+  precioTotal.innerText = pedido.reduce((acc, el)=> acc + el.precio , 0);
+}
+
+const removePedido = (id) => { 
+const item = pedido.find ( (producto) => producto.id === id);
+const indice = pedido.indexOf(item);
+pedido.splice=(indice, 1);
+
+renderPedido()
+renderCantidad()
+renderTotal()
 }
 
 //RENDERIZADO DEL PRODUCTO SELECCIONADO EN CARRITO 
+
 const renderPedido = () => {
 
   pedidoContenedor.innerHTML= ``
 
-  pedido.forEach ((item) => {
-    const div = document.createElement ('div')
-    div.classList.add('productoencarrito')
+  pedido.forEach ((producto) => {
 
+    const div = document.createElement ('div')
+
+    div.classList.add('productoencarrito')
     div.innerHTML = `
     <p>${item.nombre}</p>
     <p>$ ${item.precio}</p>
-    <button id= "eliminar${item.id}" class="btn-eliminar"> X Sustraer </button>
+    <button onclick="removePedido(${item.id})" class="btn-eliminar"> X Sustraer </button>
     `
-    
     pedidoContenedor.appendChild(div)
-
-    eliminarDelCarrito(item)
-    
+    // localStorage.setItem ('pedido', JSON.stringify(pedido))
   }) 
 }
 
-//RENDERIZADO DE LA CANTIDAD SELECCIONADA EN CARRITO
-const renderCantidad = () => {
-  contadorPedido.innerText = pedido.length
-  precioTotal.innerText = pedido.reduce((acc, el)=> acc + el.precio , 0);
-}
-
-
-function eliminarDelCarrito(item){
-  let btnEliminar = document.getElementById(`eliminar${item.id}`);
+// function eliminarDelCarrito(item){
+//   let btnEliminar = document.getElementById(`eliminar${item.id}`);
   
-  btnEliminar.addEventListener('click', ()=> {
-      btnEliminar.parentElement.remove();
-      pedido= pedido.filter(elemento=> elemento.id !== item.id);
-      renderPedido();
-      renderCantidad();
+//   btnEliminar.addEventListener('click', ()=> {
+//       btnEliminar.parentElement.remove();
+//       pedido= pedido.filter(elemento => elemento.id !== item.id);
+//       renderPedido();
+//       renderCantidad();
+//       renderTotal();
 
-      console.log(pedido)
-      //parent element me muestra al padre del elemento que tomo con id, y al hacer el remove, logro sacar al padre
-      //del contenedor de ese producto, en este caso, quien contiene mi producto es productoencarrito.
-  })
-  
-}
+//      localStorage.setItem ('pedido', JSON.stringify(pedido))
+//       //parent element me muestra al padre del elemento que tomo con id, y al hacer el remove, logro sacar al padre
+//       //del contenedor de ese producto, en este caso, quien contiene mi producto es productoencarrito.
+//   })
+// }
+
+// LOCAL STORAGE - JSON
+
+// let local
+// const pedidoenLS = JSON.parse(localStorage.getItem('pedido'))
+// if (pedidoenLS) { 
+//   local = pedidoenLS
+// renderPedido()
+// renderCantidad()
+// eliminarDelCarrito(item)
+// }
+// else {
+//  local = []
+// }
---
+++ JS/appw.js
@@ -0,0 +1,82 @@
+const productosContainer = document.querySelector('#contenedor-productosP')
+let itemW = menuWine [0]
+let pedido = []
+const pedidoContenedor = document.querySelector('#pedido-contenedor')
+const contadorPedido = document.querySelector ('#contador-pedido')
+const precioTotal = document.querySelector ('#precio-total')
+const contenedorClass = document.querySelector ('#contenedor-menu')
+
+// CREACION DE PRODUCTOS EN JS
+
+menuWine.forEach((producto) => {
+  const div = document.createElement('div')
+  div.classList.add= ('galery')
+  div.innerHTML = `
+  <img src="${producto.imagen}" alt="">
+  <h1>${producto.nombre}</h1>
+  <p>${producto.descrip}</p> 
+  <p class="precioProducto"> VALOR $ ${producto.precio}</p>
+  <button onclick="agregarPedido(${producto.id})" class="boton-pedidoAgregar">PEDIR
+  <i class="fa-solid fa-cash-register"></i></button> 
+  ` 
+  contenedorClass.appendChild(div)
+})
+
+
+// SELECCION DEL PRODUCTO
+const agregarPedido = (id) => {
+  let item = menuWine.find ((producto) => producto.id === id)
+
+  pedido.push(item)
+  console.log(pedido)
+
+  renderPedido()
+  renderCantidad()
+ 
+}
+
+//RENDERIZADO DEL PRODUCTO SELECCIONADO EN CARRITO 
+
+const renderPedido = () => {
+
+  pedidoContenedor.innerHTML= ``
+
+  pedido.forEach ((item) => {
+    const div = document.createElement ('div')
+    div.classList.add('productoencarrito')
+
+    div.innerHTML = `
+    <p>${item.nombre}</p>
+    <p>$ ${item.precio}</p>
+    <button id= "eliminar${item.id}" class="btn-eliminar"> X Sustraer </button>
+    `
+    
+    pedidoContenedor.appendChild(div)
+
+    eliminarDelCarrito(item)
+    
+  }) 
+}
+
+//RENDERIZADO DE LA CANTIDAD SELECCIONADA EN CARRITO
+const renderCantidad = () => {
+  contadorPedido.innerText = pedido.length
+  precioTotal.innerText = pedido.reduce((acc, el)=> acc + el.precio , 0);
+}
+
+
+function eliminarDelCarrito(item){
+  let btnEliminar = document.getElementById(`eliminar${item.id}`);
+  
+  btnEliminar.addEventListener('click', ()=> {
+      btnEliminar.parentElement.remove();
+      pedido= pedido.filter(elemento=> elemento.id !== item.id);
+      renderPedido();
+      renderCantidad();
+
+      console.log(pedido)
+      //parent element me muestra al padre del elemento que tomo con id, y al hacer el remove, logro sacar al padre
+      //del contenedor de ese producto, en este caso, quien contiene mi producto es productoencarrito.
+  })
+  
+}
